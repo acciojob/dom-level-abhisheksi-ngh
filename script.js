da3aba2be248b1eea50f0afcb2fbab692d567e4b
abhishekashj <abhishekkumrsingh9585@gmail.com>
2025-05-20 17:27:20 +0530

    updated

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const tar= document.getElementById('level');
+let count= +0;
+let curr= tar;
+while(curr){
+    count++;
+    curr= curr.parentElement;
+}
+alert(`The level of the element is: ${count}`);
